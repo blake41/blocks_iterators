689d48741df2a928b4699a83ddc2449a7e0796a3
Blake Johnson <blake41@gmail.com>
2014-05-06 09:45:51 -0400

    done

---
+++ blocks.rb
@@ -0,0 +1,21 @@
+# def my_method_takes_a_block
+#   yield
+# end
+
+# my_method_takes_a_block { puts "hello"}
+
+# my_method_takes_a_block do 
+#   puts "hello"
+# end
+
+# def my_method_yields_a_variable
+#   yield 7
+# end
+
+# my_method_yields_a_variable {|some_argument| puts some_argument }
+
+# def block_scope
+#   puts a
+# end
+
+# block_scope { a = 7 }
